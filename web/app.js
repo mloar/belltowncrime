8f6ab9313cbc0903dff1c1155d85582d00e7d1e4
Matthew Loar <matthew@loar.name>
2012-01-10 17:29:26 -0800

    Add some pretty.

--- web/app.js
+++ web/app.js
@@ -13,7 +13,7 @@ client.connect();
 var router = bogart.router();
 router.get('/', function (req) {
     return bogart.promisify(client.query, client)(
-        "SELECT hundred_block_location, description, COUNT(cdw_number) AS incident_count FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '7 days' AND NOT crimes.offense_code = 'X' GROUP BY hundred_block_location, crimes.offense_code, offense_descriptions.description ORDER By incident_count"
+        "SELECT hundred_block_location, description, COUNT(cdw_number) AS incident_count FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '7 days' AND NOT crimes.offense_code = 'X' GROUP BY hundred_block_location, crimes.offense_code, offense_descriptions.description ORDER By incident_count DESC"
         ).then(
         function (result) {
             return viewEngine.respond('index.jade',
