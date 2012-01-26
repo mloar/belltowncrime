b8d3dfeae1b87752e55c41308073684837eafcbc
Matthew Loar <matthew@loar.name>
2012-01-25 16:32:31 -0800

    Account for time zone.

--- web/app.js
+++ web/app.js
@@ -13,7 +13,7 @@ client.connect();
 var router = bogart.router();
 router.get('/', function (req) {
     return bogart.promisify(client.query, client)(
-        "SELECT hundred_block_location, description, COUNT(go_number) AS incident_count FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '7 days' AND NOT crimes.offense_code = 'X' GROUP BY hundred_block_location, crimes.offense_code, offense_descriptions.description ORDER By incident_count DESC"
+        "SELECT hundred_block_location, description, COUNT(go_number) AS incident_count FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '7 days 8 hours' AND NOT crimes.offense_code = 'X' GROUP BY hundred_block_location, crimes.offense_code, offense_descriptions.description ORDER By incident_count DESC"
         ).then(
         function (result) {
             return viewEngine.respond('index.jade',
@@ -25,6 +25,16 @@ router.get('/', function (req) {
 router.get('/major', function (req) {
     return bogart.promisify(client.query, client)(
         "SELECT DISTINCT hundred_block_location, description, occurred_date, go_number FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '2 months' AND (crimes.offense_code LIKE '12__' OR crimes.offense_code LIKE '130_' OR crimes.offense_code LIKE '22__' OR crimes.offense_code LIKE '9__') ORDER BY occurred_date DESC").then(
+        function (result) {
+            return viewEngine.respond('major.jade',
+            {
+                locals: result
+            });
+        });
+});
+router.get('/drugs', function (req) {
+    return bogart.promisify(client.query, client)(
+        "SELECT DISTINCT hundred_block_location, description, occurred_date, go_number FROM crimes LEFT JOIN offense_descriptions ON crimes.offense_code = offense_descriptions.offense_code WHERE occurred_date > NOW() - INTERVAL '2 months' AND crimes.offense_code LIKE '35__' ORDER BY occurred_date DESC").then(
         function (result) {
             return viewEngine.respond('major.jade',
             {
