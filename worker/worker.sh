21e3ea81413bb3253fc98b5cf937f3209306e8b6
Matt Loar <matt@loar.name>
2013-08-04 21:25:14 +0000

    Remove obselete -M option to s3cmd.

--- worker/worker.sh
+++ worker/worker.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 /home/mloar/opt/bin/node update.js
 /home/mloar/opt/bin/node liquor.js
 /home/mloar/opt/bin/node render.js
-/usr/bin/s3cmd sync -P -m 'text/html' -M out/ s3://www.belltowncrime.com/
+/usr/bin/s3cmd sync -P -m 'text/html' out/ s3://www.belltowncrime.com/
